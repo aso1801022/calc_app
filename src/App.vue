020294dd6367a35e9b4fedd4bebd2581e7b3116a
aso1801022 <2001056@s.asojuku.ac.jp>
2020-06-30 05:41:22 +0000

    first git

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+   <Calc  v-bind:title="message"
+   v-on:result-event="appAction"/>
+   <hr>
+   <div><table v-html="log"></table></div>
+  </div>
+</template>
+
+<script>
+
+import Calc from './components/Calc.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Calc
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
